7d3dfa36263e5f645e46557e9e46063c3f677d4b
tristandelemos <71288767+tristandelemos@users.noreply.github.com>
2023-05-24 10:24:08 -0700

    added test cases

--- memSim.py
+++ memSim.py
@@ -1,4 +1,40 @@
+#!usr/bin/env python3
 """
 Created by Tristan de Lemos and Trenten Spicer
 Simulates virtual memory by translating virtual memory addresses to physical memory addresses
 """
+
+import sys
+
+
+def hardcoded():
+    pass
+
+
+def main():
+
+    algorithm = "FIFO"
+    frames = 256
+    # check for algorithm
+    numOfArgs = len(sys.argv)
+    if numOfArgs > 1:
+        file = open(str(sys.argv[1]), "r")
+    else:
+        file = open("fifo1.txt", "r")
+
+    hardcoded()
+
+
+    """
+    if numOfArgs > 4 or numOfArgs == 1:
+        print("Usage: memSim <reference.txt> <FRAME_SIZE> <ALGORITHM>")
+        return 0
+
+    if numOfArgs > 2:
+        frames = int(sys.argv[2])
+
+    if numOfArgs > 3:
+        algorithm = sys.argv[3]
+
+    """
+    return 0
