11733a14cbfd047888a0e5312f6e996c6816475e
EternalLabrynth <eternallabrynth@gmail.com>
2023-05-26 12:31:11 -0700

    stuff

--- memSim.py
+++ memSim.py
@@ -7,16 +7,28 @@ Simulates virtual memory by translating virtual memory addresses to physical mem
 import sys
 
 
+def add_to_tlb(tlb, page_num, frame):
+    if (len(tlb) < 16):
+        #append to end of fifo queue
+        tlb.append((page_num, frame))
+    else:
+        #remove oldest, then append
+        tlb.pop(0)
+        tlb.append((page_num, frame))
+
+    return
+
+
+
 def hardcoded(file, tlb, ptable):
     lines = file.readlines()
     frame = 0
     # go through each line of the file
     for line in lines:
         # take out only first four bits to find page number
-        print(line, end=" ")
         virtual = bin(int(line))
         virtual = virtual[2:]
-        page_num = int(virtual[0:8], 2)
+        page_num = virtual[0:7]
         # check TLB for frame num
         in_tlb = False
         framenum = -1
@@ -29,36 +41,19 @@ def hardcoded(file, tlb, ptable):
 
         else:
             # if not in TLB check in page table
-            print(page_num)
-            entry = ptable[page_num]
+            entry = ptable[int(page_num)]
             if entry[2] != -1:
                 frame = entry[1]
                 # put into TLB
-                if (len(tlb) < 16):
-                    # append to end of fifo queue
-                    tlb.append((page_num, frame))
-                else:
-                    # remove oldest, then append
-                    tlb.pop(0)
-                    tlb.append((page_num, frame))
+                add_to_tlb(tlb, page_num, frame)
 
             else:
                 # if not in page table, check BACKING_STORE.bin
-                backend = open('BACKING_STORE.bin', 'rb')
-                i = 0
-                while True:
-                    try:
-                        l = next(backend)
-                        #if i == page_num:
-                        print(l)
-                        i += 1
-                        print(i)
-                    except StopIteration:
-                        break
-                backend.close()
+                pass
 
 
 def main():
+
     tlb = []
 
     ptable = [-1, -1, -1]
@@ -68,13 +63,15 @@ def main():
     frames = 256
     # check for algorithm
 
+
+    
     numOfArgs = len(sys.argv)
     if numOfArgs > 1:
         file = open(str(sys.argv[1]), "r")
     else:
         file = open("fifo1.txt", "r")
 
-    hardcoded(file, tlb, ptable)
+    hardcoded(file)
 
     """"
     
