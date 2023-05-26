a8b155e02f704db12c9b5a022c6c60f2c75024df
tristandelemos <71288767+tristandelemos@users.noreply.github.com>
2023-05-26 12:26:24 -0700

    update

--- memSim.py
+++ memSim.py
@@ -13,9 +13,10 @@ def hardcoded(file, tlb, ptable):
     # go through each line of the file
     for line in lines:
         # take out only first four bits to find page number
+        print(line, end=" ")
         virtual = bin(int(line))
         virtual = virtual[2:]
-        page_num = virtual[0:7]
+        page_num = int(virtual[0:8], 2)
         # check TLB for frame num
         in_tlb = False
         framenum = -1
@@ -28,25 +29,36 @@ def hardcoded(file, tlb, ptable):
 
         else:
             # if not in TLB check in page table
-            entry = ptable[int(page_num)]
+            print(page_num)
+            entry = ptable[page_num]
             if entry[2] != -1:
                 frame = entry[1]
                 # put into TLB
                 if (len(tlb) < 16):
-                    #append to end of fifo queue
+                    # append to end of fifo queue
                     tlb.append((page_num, frame))
                 else:
-                    #remove oldest, then append
+                    # remove oldest, then append
                     tlb.pop(0)
                     tlb.append((page_num, frame))
 
             else:
                 # if not in page table, check BACKING_STORE.bin
-                pass
+                backend = open('BACKING_STORE.bin', 'rb')
+                i = 0
+                while True:
+                    try:
+                        l = next(backend)
+                        #if i == page_num:
+                        print(l)
+                        i += 1
+                        print(i)
+                    except StopIteration:
+                        break
+                backend.close()
 
 
 def main():
-
     tlb = []
 
     ptable = [-1, -1, -1]
@@ -56,15 +68,13 @@ def main():
     frames = 256
     # check for algorithm
 
-
-    
     numOfArgs = len(sys.argv)
     if numOfArgs > 1:
         file = open(str(sys.argv[1]), "r")
     else:
         file = open("fifo1.txt", "r")
 
-    hardcoded(file)
+    hardcoded(file, tlb, ptable)
 
     """"
     
