96aa00d96b8018726d1942e6ac4531e08466549f
EternalLabrynth <eternallabrynth@gmail.com>
2023-05-26 18:14:05 -0700

    revamped

--- memSim.py
+++ memSim.py
@@ -31,8 +31,8 @@ def add_to_memory(memory, frame, max_frames, frame_num):
 def hardcoded(file, tlb, ptable, memory, frames, algorithm):
 
     LRU_i = 0
+    fifo_i = 0
     total = 0
-    frame_num = 0
     frame = 0
     block = 0
     byte = 0
@@ -53,24 +53,32 @@ def hardcoded(file, tlb, ptable, memory, frames, algorithm):
         in_tlb = False
         framenum = -1
 
+        print(tlb)
         for pair in tlb:
             if pair[0] == page_num:
                 in_tlb = True
                 framenum = pair[1]
                 hits += 1
+                print("tlb hit")
         if in_tlb:
             #printing info
             block = memory[framenum]
             frame = framenum
             byte = block[offset]
 
+            #update process usage parameter / valid bit
+            if algorithm == "LRU":
+                old_entry = ptable[page_num]
+                ptable[page_num] = (old_entry[0], LRU_i)
+                LRU_i += 1
+
         #     print(framenum)
 
         # if not in TLB check in page table
         else:
             entry = ptable[page_num]
-            if entry[2] != -1:
-                frame = entry[1]
+            if entry[1] != -1:
+                frame = entry[0]
                 add_to_tlb(tlb, page_num, frame)
 
                 #update process usage parameter / valid bit
@@ -82,6 +90,7 @@ def hardcoded(file, tlb, ptable, memory, frames, algorithm):
                 #printing info
                 block = memory[frame]
                 byte = block[offset]
+                print("ptable hit")
 
             else:
                 # if not in page table, check BACKING_STORE.bin
@@ -92,53 +101,90 @@ def hardcoded(file, tlb, ptable, memory, frames, algorithm):
                 #while num pages < page table size
                 while (i != 256):
                     try:
+                        #read frame
                         l = backend.read(256)
+                        #if frame is page
                         if i == page_num:
+                            #list of bytes
                             block = list(l)
                             byte = block[offset]
                             if byte > 127:
                                 byte = 256 - byte
                                 byte = byte * -1
-                            #add to page table / main memory
-                            add_to_memory(memory, l, frames, frame_num)
-                            if algorithm == "FIFO" or algorithm == "LRU":
-                                #update page table with new page
-                                ptable[page_num] = (frame_num, LRU_i)
-                                #update prints
-                                frame = frame_num
-                                #increase num pages added
-                                LRU_i += 1
-                                #if more pages than frames
-                                if LRU_i >= frames:
-                                    #determine oldest frame (earliest use and/or placement)
-                                    min = -1
-                                    min_i = -1
-                                    for i in range(len(ptable)):
-                                        if min == -1:
-                                            min = ptable[i][1]
-                                            min_i = i
-                                        if ptable[i][1] < min:
-                                            min = ptable[i][1]
-                                            min_i = i
-                                    #next frame to be updated will be least recently used
-                                    frame_num = min_i
-                            #add to tlb
-                            add_to_tlb(tlb, page_num, frame_num)
+                            
+                            
+                            if algorithm == "FIFO":
+                                print("FIFO")
+                                #check for flip around
+                                if fifo_i >= frames:
+                                    fifo_i = 0
+                                #invalidate previous frame's ptable
+                                for i in range(len(ptable)):
+                                    if ptable[i][0] == fifo_i:
+                                        ptable[i] = (fifo_i, -1)
+                                #overwrite frame
+                                add_to_memory(memory, l, frames, fifo_i)
+                                #add to page table
+                                ptable[page_num] = (fifo_i, 1)
+                                frame = fifo_i
+                                fifo_i += 1
 
-                            #increment
-                            if LRU_i < frames:
-                                frame_num += 1
+                            if algorithm == "LRU":
+                                #check if memory filled
+                                if LRU_i >= frames:
+                                    #find least recently used frame
+                                    min = -1
+                                    for entry in ptable:
+                                        if entry[1] != -1:
+                                            if min == -1 or entry[1] < min:
+                                                #this entry is oldest thus far
+                                                min = entry[1]
+                                                min_entry = entry
+                                    #overwrite LRU frame and invalidate prev entry
+                                    print("min entry:", min_entry)
+                                    for i in range(len(ptable)):
+                                        if ptable[i] == min_entry:
+                                            ptable[i] = (ptable[i][0], -1)
+                                    add_to_memory(memory, l, frames, min_entry[0])
+                                    #add to page table
+                                    ptable[page_num] = (min_entry[0], LRU_i)
+                                    frame = min_entry[0]
+                                else:
+                                    #memory not yet filled
+                                    #add to memory
+                                    add_to_memory(memory, l, frames, LRU_i)
+                                    #add to page table
+                                    ptable[page_num] = (LRU_i, LRU_i)
+                                    frame = LRU_i
+                                #increment lru i
+                                LRU_i += 1
+                                # for entry in ptable:
+                                #     if entry != [-1, -1, -1]:
+                                #         print("ptable entry:", entry)
+
+                                
+                                #remove from tlb table
+                                for i in range(len(tlb)):
+                                    if tlb[i][1] == frame:
+                                        tlb.pop(i)
+
+                                #add to tlb
+                                add_to_tlb(tlb, page_num, frame)
                         i += 1
                     except StopIteration:
                         break
                 backend.close()
 
+            for entry in memory:
+                if entry != -1:
+                    print("entry:", entry[0:4])
+
         # print third part of printout
         print(f'{int(line)}, {byte}, {frame},')
-        for index in block:
-            print(f'{index:X}', end='')
+        # for index in block:
+        #     print(f'{index:X}', end='')
 
-        print("total:", total)
+        # print("total:", total)
         # frame += 1
         total += 1
 
@@ -148,7 +194,7 @@ def hardcoded(file, tlb, ptable, memory, frames, algorithm):
     print(f'Page Fault Rate = {faults/total:.3f}')
     print(f'TLB Hits = {hits}')
     print(f'TLB Misses = {total - hits}')
-    print(f'TLB Hit Rate = {1 - (faults/total):.3f}')
+    print(f'TLB Hit Rate = {(hits/total):.3f}')
 
 
 
@@ -214,9 +260,6 @@ def main():
 
     memory = [-1] * frames
     # print("memory:", len(memory))
-
-    hardcoded(file, tlb, ptable, memory, frames, algorithm)
-
     # print(memory)
 
 
@@ -234,8 +277,38 @@ def main():
     if numOfArgs > 3:
         algorithm = sys.argv[3]
 
+    
+    hardcoded(file, tlb, ptable, memory, frames, algorithm)
+
+
     return 0
 
 
 if __name__ == '__main__':
     main()
+
+
+
+# #update page table with new page
+#                                 ptable[page_num] = (frame_num, LRU_i)
+#                                 #update prints
+#                                 frame = frame_num
+                                
+#                                 #if more pages than frames
+#                                 print("lru i:", LRU_i)
+#                                 if LRU_i >= frames:
+#                                     #determine oldest frame (earliest use and/or placement)
+#                                     min = -1
+#                                     min_i = -1
+#                                     for i in range(len(ptable)):
+#                                         if ptable[i] != [-1, -1, -1]:
+#                                             print("current page", ptable[i])
+#                                             if min == -1:
+#                                                 min = ptable[i][1]
+#                                                 min_i = ptable[i][0]
+#                                             elif ptable[i][1] < min:
+#                                                 min = ptable[i][1]
+#                                                 min_i = ptable[i][0]
+#                                     #next frame to be updated will be least recently used
+#                                     frame_num = min_i
+#                                     print("frame num / min_i:", frame_num)
